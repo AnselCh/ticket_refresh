34a9be5135bbbd73166a02e6d4d691aa7ab0294c
ansel <ansel@ansel.nb>
2023-01-11 20:37:02 +0800

    exe

--- ticket.py
+++ ticket.py
@@ -1,8 +1,7 @@
-import requests
-import time
+import requests,time,sys,random
 from bs4 import BeautifulSoup
 from PyQt5 import QtWidgets
-import sys
+
 
 
 def TS1():
@@ -39,10 +38,29 @@ def win():
     sys.exit(app.exec_())
 
 
+headerlist = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991",
+           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 OPR/42.0.2393.94",
+           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36 OPR/47.0.2631.39",
+           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+           "Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+           "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+           "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+            ]
+#隨機選擇headers
+user_agent = random.choice(headerlist)
+headers = {'User-Agent': user_agent}
 while True:
-    response = requests.get(
-        "https://tixcraft.com/ticket/area/23_megaport/13729")
-
+    print('connecting...')
+    try:
+        response = requests.get(
+            "https://tixcraft.com/ticket/area/23_megaport/13729", headers=headers)
+    except:
+        print('error url : https://tixcraft.com/ticket/area/23_megaport/13729')
     soup = BeautifulSoup(response.text, "html.parser")
     ticket_status = soup.find('div', class_='zone area-list')
     T1 = ticket_status.find('ul', id='group_20944')
@@ -50,13 +68,9 @@ while True:
     T3 = ticket_status.find('ul', id='group_20946')
     T4 = ticket_status.find('ul', id='group_20947')
 
-    if TS1() != None:
+    if TS1() != None or TS2() != None or TS3() != None or TS4() != None:
         win()
-    if TS2() != None:
-        print(T2.text)
-    if TS3() != None:
-        print(T3.text)
-    if TS4() != None:
-        print(T4.text)
-
-    time.sleep(10)
+        break
+    print(f'{T1.text}\n{T2.text}\n{T3.text}\n{T4.text}')
+    time.sleep(2)
+    time.sleep(random.randint(1,11))
