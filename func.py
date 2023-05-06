4c629e1d70c3361f92aabdc9375a5494a95e798a
showay <df405102@icloud.com>
2023-05-06 18:56:26 +0800

    Add: line bot notification

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 from bs4 import BeautifulSoup
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import *
@@ -6,20 +7,70 @@ from PyQt5.QtCore import *
 
 class AutoRun:
 
-    def __init__(self, area_id_start, area_id_end) -> None:
+    def __init__(
+            self,
+            url: str,
+            area_id_start: int,
+            area_id_end: int,
+            config: dict) -> None:
         self.header = self.read_header()
+        self.url = url
+        self.title = None
         self.area_id_start = area_id_start
         self.area_id_end = area_id_end
+        self.notification_type = config.get("notification_type") or {}
+        self.token = config["token"]
 
     def read_header(self) -> str:
-        
         pass
 
     def get_header(self) -> dict:
         try:
             return {'User-Agent': self.header}
         except: pass
-    def has_ticket_alarm(self, ts: list) -> None:
+    
+    def __del__(self) -> None:
+        self._send_line_msg("End Monitoring.\n")
+
+    def parse_title(self, soup:BeautifulSoup) -> None:
+        h2s = soup.find_all('h2', {"class": "activityT title"})
+        if h2s:
+            self.title = h2s[0].text
+            self._send_line_msg("Start Monitoring.\n")
+
+    def has_ticket_alarm(self, ts:list) -> None:
+        if self.notification_type.get("line"):
+            self._has_ticket_alarm_line(ts)
+        if self.notification_type.get("Window") is False:
+            self._has_ticket_alarm_qt(ts)
+
+    def _has_ticket_alarm_line(self, ts:list=[], msg_overwrite="") -> None:
+        remain_ticket = []
+        for t in ts:
+            if 'remain' in t:
+                remain_ticket.append(f''.join(t))    
+        msg_body = "".join(remain_ticket)
+        self._send_line_msg(msg_body)
+
+    def _send_line_msg(self, msg_body):
+        token = self.token["line"]
+        assert type(token) is str
+        assert len(token) > 0
+        # https://notify-bot.line.me/my/
+        headers = {
+            "Authorization": "Bearer " + token, 
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+
+        msg = f"[Ticket Monitor] \n{self.title}\n\n"
+        msg += f"{msg_body}"
+        msg += f"\nURL: {self.url}"
+            
+        payload = {'message': msg }
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+        return r.status_code
+    
+    def _has_ticket_alarm_qt(self, ts: list) -> None:
         app = QtWidgets.QApplication(sys.argv)
         Form = QtWidgets.QWidget()
         Form.setWindowFlags(Qt.WindowStaysOnTopHint)
@@ -49,12 +100,13 @@ class AutoRun:
         sys.exit(app.exec_())
 
     def check_ticket_status(self, soup: BeautifulSoup):
+        if self.title is None:
+            self.parse_title(soup)
         ticket_status = soup.find('div', class_='zone area-list')
         ticket_status_list = []
         find_flag = False
         # print(ticket_status)
         for i in range(self.area_id_start, self.area_id_end + 1):  # area id range
-
             try:
                 group_ul = ticket_status.find('ul', id=f'group_{i}')
                 lis = group_ul.find_all('li')
@@ -65,6 +117,7 @@ class AutoRun:
                         find_flag = True
             except:
                 return True, ticket_status_list, True  # 當找不到tag返回out_of_range==True
+
         if find_flag:
             self.has_ticket_alarm(ticket_status_list)
             return False, ticket_status_list, False
--- main.py
+++ main.py
@@ -1,20 +1,59 @@
-import requests
 import time
+import random
+import json
+from datetime import datetime
+
+import requests
+
 from func import AutoRun
 from bs4 import BeautifulSoup
-from datetime import datetime
-import random
 
+def get_config():
+    import sys
+    import os.path
+    CWD = os.path.abspath(os.path.dirname(sys.executable))
+    config_path = os.path.join(CWD, "config.json")
+    try:
+        with open(config_path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError as e:
+        print(f"Not getting config from {config_path}. "\
+              "Assume running with source code. "\
+              "Will be using ./config.json instead.")
+        with open("config.json", "r") as f:
+            return json.load(f)
 
+def isNone(val):
+    return val is None
+
+def get_value_or_input(target, field, input_question):
+    val = target.get(field)
+    valid = False
+    if type(val) is int:
+        valid = True if val > 0 else False
+    if type(val) is str:
+        valid = True if val else False
+    if valid:
+        return val
+    
+    answer = input(input_question)
+    try:
+        return int(answer)
+    except ValueError:
+        return answer
+    
 def main():
+    config = get_config()
     not_find_flag = True
     out_of_range = False
-    url = str(input('請輸入網址:'))
-    area_id_start = int(input("搜尋起點:"))
-    area_id_end = int(input("搜尋終點:"))
+    target = config.get("target") or {}
+    url = get_value_or_input(target, "url", '請輸入網址: ')
+    area_id_start = get_value_or_input(target, "start", "搜尋起點: ")
+    area_id_end = get_value_or_input(target, "end", "搜尋終點: ")
 
+    ar = AutoRun(url, area_id_start, area_id_end, config)
     while not_find_flag:
-        ar = AutoRun(area_id_start, area_id_end)
+        
         print('connecting...')
         try:
             response = requests.get(
